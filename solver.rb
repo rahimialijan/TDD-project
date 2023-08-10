0a95bff8b94853f82fe5ba843fdc32b38687927e
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-08-10 18:48:27 +0500

    add unit test for fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -1,15 +1,18 @@
-class Solver   
-        def factorial(number)
-          if number.negative?
-            raise 'Negative number'
-          elsif number.zero?
-            1
-          else
-            number * factorial(number - 1)
-          end
-        end
-      
-        def reverse(string)
-          string.reverse
-        end
+class Solver
+  def factorial(number)
+    if number.negative?
+      raise 'Negative number'
+    elsif number.zero?
+      1
+    else
+      number * factorial(number - 1)
+    end
+  end
+
+  def reverse(string)
+    string.reverse
+  end
+
+  def fizzbuzz
+  end
 end
