a67c778f2ecd6c030e9aaf713f22c19c8f48ef6c
Enoisong <enobongitiaba988@gmail.com>
2023-08-10 06:45:33 -0700

    add factorial and reverse methods

--- solver.rb
+++ solver.rb
@@ -1,2 +1,15 @@
-class Solver
+class Solver   
+        def factorial(number)
+          if number.negative?
+            raise 'Negative number'
+          elsif number.zero?
+            1
+          else
+            number * factorial(number - 1)
+          end
+        end
+      
+        def reverse(string)
+          string.reverse
+        end
 end
